84e09211faede835a6b0a980a4ee653a20b6e9bf
avi <avi@gmail.com>
2025-10-30 14:50:31 -0500

    holi

---
+++ ejer1_sem11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1_sem11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] PrecioProducto = new double[5];
+            int[] Cantidad = new int[5];
+            int contadorP, contadorC;
+            Console.WriteLine("------------------ HOLI BIENVENIDO ------------------");
+
+            Console.WriteLine("\nINGRESE LOS PRECIOS DE 5 PRODUCTOS.");
+
+            for (contadorP = 0; contadorP < 5; contadorP++)
+            {
+                do
+                {
+                    Console.Write("\nIngrese el precio del producto " + (contadorP + 1) + ":  ");
+                    PrecioProducto[contadorP] = Convert.ToDouble(Console.ReadLine());
+                    if (PrecioProducto[contadorP] > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR, ingrese cantidades positivas");
+                    }
+                } while (true);
+            }
+
+            Console.WriteLine("\nAHORA INGRESE SUS RESPECTIVAS CANTIDADES.");
+
+            for (contadorC = 0; contadorC < 5; contadorC++)
+            {
+                do
+                {
+
+                    Console.Write("\nIngrese las cantidad " + (contadorC + 1) + ":  ");
+                    Cantidad[contadorC] = Convert.ToInt32(Console.ReadLine());
+                    if (Cantidad[contadorC] > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR, ingrese cantidades positivas");
+                    }
+                } while (true);
+
+            }
+
+            Console.WriteLine("\nMULTIPLICACION DEL PRECIO Y SU RESPECTIVA CANTIDAD");
+
+            for (contadorP = 0; contadorP < 5; contadorP++)
+            {
+                double multiplica = PrecioProducto[contadorP] * Cantidad[contadorP];
+                Console.WriteLine("\nMultiplicación  " + (contadorP + 1) + ":  " + multiplica);
+            }
+        }
+    }
+}
---
+++ ejer2_sem11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer2_sem11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a_producto = new string[10];
+            string nom_producto;
+            int producto_r;
+
+            Console.Write("BIENVNIDO ACA PODRA INGRESAR EL NOMBRE DE 10 PRODUCTOS\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    producto_r = 0;
+                    Console.Write("\nIngrese nombre del producto {0}: ", i + 1);
+                    nom_producto = Console.ReadLine().ToUpper();
+
+                    if (i == 0)
+                    {
+                        a_producto[i] = nom_producto;
+                    }
+
+                    else
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            if (a_producto[j] == nom_producto)
+                            {
+                                Console.WriteLine("El nombre del producto se a repetido");
+                                producto_r = 1;
+                            }
+                        }
+
+                        if (producto_r == 0)
+                        {
+                            a_producto[i] = nom_producto;
+                        }
+                    }
+
+                } while (producto_r != 0);
+            }
+        }
+    }
+}
